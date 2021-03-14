1d48a3988d904e45c0b9e8d7b5ef3ef08769e56b
Toggy <tognolamontes@yahoo.com.br>
2021-03-14 18:09:33 -0300

    implementação do metodo para usuario criar Plano de Conta ( sem tratamento de exceção)

--- src/main/java/com/example/app/repository/UsuarioRepository.java
+++ src/main/java/com/example/app/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.example.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.app.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	public Optional<Usuario> findById(Integer id);
 }
