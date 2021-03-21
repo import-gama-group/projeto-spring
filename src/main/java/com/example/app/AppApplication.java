cd7b3256cb563580ce2883844bda43bf437e9c5b
Toggy <tognolamontes@yahoo.com.br>
2021-03-21 12:52:34 -0300

    removed initializer user

--- src/main/java/com/example/app/AppApplication.java
+++ src/main/java/com/example/app/AppApplication.java
@@ -1,11 +1,7 @@
 package com.example.app;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.example.app.TestComponent;
 
 @SpringBootApplication
 public class AppApplication {
@@ -14,11 +10,4 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 		
 	}
-	@Bean
-    public CommandLineRunner run(TestComponent t) throws Exception {
-        return args -> {
-        	t.test();
-        };
-	}
-
 }
