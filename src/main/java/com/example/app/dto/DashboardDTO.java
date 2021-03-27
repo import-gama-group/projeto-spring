1aa6192dc8e8c2c46e2897ee8bf730cc0fceaef5
Ricardo Tognola Montes de Oca <43882173+Toggy81@users.noreply.github.com>
2021-03-27 09:09:11 -0300

    Update DashboardDTO.java

--- src/main/java/com/example/app/dto/DashboardDTO.java
+++ src/main/java/com/example/app/dto/DashboardDTO.java
@@ -1,14 +1,9 @@
 package com.example.app.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-@JsonPropertyOrder({ "conta_debito", "conta_credito"})
 public class DashboardDTO {
 	
-    @JsonProperty("conta_debito")
 	private ContaDTO ContaDebito;
-    @JsonProperty("conta_credito")
+
 	private ContaDTO ContaCredito;
 	
 	public ContaDTO getContaDebito() {
